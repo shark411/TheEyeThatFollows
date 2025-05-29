e81e4e042de4d3a0bc05da359f807da81d63f604
shark411 <abigail.rose.byrnes@gmail.com>
2025-05-28 20:43:32 -0400

    EYE looks TOP LEFT if you click there.

--- TheEyeThatFollows/Eye.cs
+++ TheEyeThatFollows/Eye.cs
@@ -19,8 +19,14 @@ namespace TheEyeThatFollows
         private Color _eyeColour; //To make the eye change colours
         private string _eyeName; //The name of the eye
         private SpriteFont _GameFont; //Our font
+
         private bool _isBlinking; //To check if it's blinking
         private float _blinkTimer; //So it isnt always blinking
+        private bool _lookingTopLeft; //WHERE IS THE EYE LOOKING
+        private bool _lookingTopRight; //WHERE IS THE EYE LOOKING
+        private bool _lookingBottomLeft; //WHERE IS THE EYE LOOKING
+        private bool _lookingBottomRight; //WHERE IS THE EYE LOOKING
+        private float _lookTimer; //So it reverts to "normal"
 
         //Constructor
         public Eye(String eyeName, Texture2D iris, Texture2D eyelid, Texture2D eyebase, SpriteFont GameFont)
@@ -34,6 +40,28 @@ namespace TheEyeThatFollows
             _eyebase = eyebase;
             _isBlinking = false;
             _blinkTimer = 60;
+            _lookingTopLeft = false;
+            _lookingTopRight = false;
+            _lookingBottomLeft = false;
+            _lookingBottomRight = false;
+            _lookTimer = 30;
+        }
+
+        public Rectangle TopLeft() //WHERE IS THE EYE LOOKING
+        {
+            return new Rectangle(-400, -240, 800, 480);
+        }
+        public Rectangle TopRight() //WHERE IS THE EYE LOOKING
+        {
+            return new Rectangle(-400, -240, 800, 480);
+        }
+        public Rectangle BottomLeft() //WHERE IS THE EYE LOOKING
+        {
+            return new Rectangle(-400, -240, 800, 480);
+        }
+        public Rectangle BottomRight() //WHERE IS THE EYE LOOKING
+        {
+            return new Rectangle(-400, -240, 800, 480);
         }
 
         public Rectangle GetBounds() //Eye Hitbox
@@ -51,6 +79,14 @@ namespace TheEyeThatFollows
             {
                 _isBlinking = false;
             }
+            _lookTimer--;
+            if (_lookTimer <= 0) //Dont always look!!!
+            {
+                _lookingTopLeft = false;
+                _lookingTopRight = false;
+                _lookingBottomLeft = false;
+                _lookingBottomRight = false;
+            }
 
             MouseState currentMouseState = Mouse.GetState();
             if (currentMouseState.LeftButton == ButtonState.Pressed)
@@ -62,13 +98,35 @@ namespace TheEyeThatFollows
                 }
                 //No else because that makes it stop blinking when you click away!
                 //The eye should feel pain.
+                if (TopLeft().Contains(currentMouseState.Position) && !(GetBounds().Contains(currentMouseState.Position))) //If you tap, the eye will see!
+                {
+                    _lookingTopLeft = true;
+                    _lookingTopRight = false;
+                    _lookingBottomLeft = false;
+                    _lookingBottomRight = false;
+                    _lookTimer = 30;
+                }
+                else //Theres nothing to look at!
+                {
+                    _lookingTopLeft = false;
+                    _lookingTopRight = false;
+                    _lookingBottomLeft = false;
+                    _lookingBottomRight = false;
+                }
             }
             }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(_eyebase, new Vector2(350, 180), Color.White); //EYEWHITE FIRST
-            spriteBatch.Draw(_iris, new Vector2(350, 180), _eyeColour); //IRIS SECOND
+            if (_lookingTopLeft == true) //LOOKING TOP LEFT
+            {
+                spriteBatch.Draw(_iris, new Vector2(340, 170), _eyeColour);
+            }
+            else //Hey! we have nothing to look at, we'll look at YOU!
+            {
+                spriteBatch.Draw(_iris, new Vector2(350, 180), _eyeColour);
+            }
             if (_isBlinking == true) //If it's blinking, lets actually blink!
             {
                 spriteBatch.Draw(_eyelid, new Vector2(350, 180), Color.White);
